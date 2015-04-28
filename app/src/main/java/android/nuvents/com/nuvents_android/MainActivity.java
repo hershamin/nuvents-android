32ecdd7047e3f0e2fcb3fc23470a01bbdfb7db72
Hersh Amin <h.amin@utexas.edu>
2015-04-28 14:58:19 -0500

    Added default google maps setup

--- app/src/main/java/android/nuvents/com/nuvents_android/MainActivity.java
+++ app/src/main/java/android/nuvents/com/nuvents_android/MainActivity.java
@@ -4,16 +4,33 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
 
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions().title("Sydney")
+                .snippet("The most populus city in Australia.")
+                .position(sydney));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
