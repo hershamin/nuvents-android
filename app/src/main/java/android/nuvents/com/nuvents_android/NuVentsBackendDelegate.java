11f56b2653e9e24e45ccb273e29e9e8c7cd1007b
Hersh Amin <h.amin@utexas.edu>
2015-04-28 16:19:35 -0500

    Completed listener functions, Added json simple library to return objects from the server as JSON

--- app/src/main/java/android/nuvents/com/nuvents_android/NuVentsBackendDelegate.java
+++ app/src/main/java/android/nuvents/com/nuvents_android/NuVentsBackendDelegate.java
@@ -4,20 +4,22 @@ package android.nuvents.com.nuvents_android;
  * Created by hersh on 4/28/15.
  */
 
+import org.json.simple.JSONObject;
+
 public interface NuVentsBackendDelegate {
 
     // Client Request
-    public void nuventsServerDidReceiveNearbyEvent();
-    public void nuventsServerDidReceiveEventDetail();
+    public void nuventsServerDidReceiveNearbyEvent(JSONObject event);       // Got nearby event
+    public void nuventsServerDidReceiveEventDetail(JSONObject event);       // Got event detail
 
     // Connection Status
-    public void nuventsServerDidGetNewData();
-    public void nuventsServerDidConnect();
-    public void nuventsServerDidDisconnect();
-    public void nuventsServerDidRespondToPing();
+    public void nuventsServerDidGetNewData(String channel, Object data);    // Got new data from any WS event
+    public void nuventsServerDidConnect();                                  // Connected
+    public void nuventsServerDidDisconnect();                               // Disconnected
+    public void nuventsServerDidRespondToPing(String response);             // Got ping response
 
     // Client Request Status & Other errors
-    public void nuventsServerDidReceiveError();
-    public void nuventsServerDidReceiveStatus();
+    public void nuventsServerDidReceiveError(String type, String error);    // Error
+    public void nuventsServerDidReceiveStatus(String type, String status);  // Status
 
 }
