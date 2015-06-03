d29cdf1ade1b93531216b20e4e1f983efc171828
Hersh Amin <h.amin@utexas.edu>
2015-06-03 16:45:54 -0500

    copied listView logic from welcomeActivity

--- app/src/main/java/android/nuvents/com/nuvents_android/ListActivity.java
+++ app/src/main/java/android/nuvents/com/nuvents_android/ListActivity.java
@@ -1,17 +1,108 @@
 package android.nuvents.com.nuvents_android;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import org.json.simple.JSONObject;
+
+import java.util.Map;
 
 
 public class ListActivity extends ActionBarActivity {
+
+    WebView webView;
+    EditText searchField;
+    LinearLayout mainLinLay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        // Init vars
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.setWebViewClient(new UIWebView());
+        searchField.addTextChangedListener(searchFieldWatcher);
+        mainLinLay = (LinearLayout) findViewById(R.id.mainLinLay);
+        mainLinLay.setOnTouchListener(screenTouchListener);
+    }
+
+    // Dismiss text field on clicks anywhere other than keyboard
+    View.OnTouchListener screenTouchListener = new View.OnTouchListener(){
+        @Override
+        public boolean onTouch(View view, MotionEvent ev) {
+            InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            in.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            return false;
+        }
+    };
+
+    // Search field changed value
+    TextWatcher searchFieldWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            //
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            boolean searchProcess = GlobalVariables.searchProc;
+            if (!searchProcess) { // Search process free
+                searchProcess = true;
+                String searchText = searchField.getText().toString();
+                webView.loadUrl("javascript:searchByTitle('" + searchText + "')");
+                searchProcess = false;
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            //
+        }
+    };
+
+    // Webview delegate methods
+    private class UIWebView extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+            if (url.contains("opendetailview://")) {
+                String eid = url.split("//")[1];
+                openDetailView(eid);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // Convert hashmap to json
+            Map<String, JSONObject> eventMap = GlobalVariables.eventJson;
+            JSONObject eventsJson = new JSONObject();
+            for (String key : eventMap.keySet()) {
+                eventsJson.put(key, eventMap.get(key));
+            }
+            // Send events to listview
+            view.loadUrl("javascript:setEvents(" + eventsJson + ")");
+            super.onPageFinished(view, url);
+        }
     }
 
     @Override
